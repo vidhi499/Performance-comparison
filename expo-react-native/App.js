102ad1476c149d2b55232053fe78b88c5455a8a2
Vidhi Kataria <vidhikataria@Vidhis-MacBook-Pro.local>
2021-12-02 19:14:35 +0530

    expo react native form added

--- expo-react-native/App.js
+++ expo-react-native/App.js
@@ -1,12 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View
+        style={{
+          flexDirection: "row",
+
+          padding: 20,
+        }}
+      >
+        <View style={{ backgroundColor: "#f4f4f5", padding: "30px" }}>
+          <Text style={{ color: "black", paddingBottom: "10px" }}>
+            Enter Email
+          </Text>
+          <TextInput style={styles.input} placeholder="Enter Email" />
+          <Text style={{ color: "black", paddingBottom: "10px" }}>
+            Enter Password
+          </Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Enter Password"
+            textContentType="password"
+          />
+          <Button
+            title="Submit"
+            color="black"
+            onPress={() => alert("Submit button pressed")}
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -14,8 +39,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  input: {
+    height: 40,
+    width: 300,
+    borderColor: "#a1a1aa",
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 4,
+    marginBottom: "20px",
   },
 });

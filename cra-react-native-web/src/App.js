8e09d9bdf774844e29e98234e79f3b05672128e6
Suraj Ahmed <suraj@geekyants.com>
2021-12-02 18:53:40 +0530

    Modified UI react-native-web

--- cra-react-native-web/src/App.js
+++ cra-react-native-web/src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <View
+      style={{
+        width: "100%",
+        height: "100vh",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <View
+        style={{
+          backgroundColor: "gray",
+          width: "25%",
+          margin: "auto",
+          padding: "30",
+          borderRadius: "4",
+        }}
+      >
+        <View
+          style={{
+            height: "100%",
+            width: "100%",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <View id="email">
+            <Text>Email address</Text>
+            <TextInput type="email" />
+            <Text>We'll never share your email.</Text>
+          </View>
+          <View id="password" pt="8">
+            <Text>Password</Text>
+            <TextInput type="password" />
+          </View>
+          <View
+            style={{
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <TouchableOpacity
+              style={{
+                paddingHorizontal: 10,
+                marginTop: 8,
+                marginHorizontal: "auto",
+                backgroundColor: "blue",
+              }}
+              type="submit"
+              variant="outline"
+            >
+              Submit
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </View>
   );
 }
 

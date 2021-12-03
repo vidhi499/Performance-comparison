b27100079edc3cbf3aa8a45f931a78950fa577f1
Suraj Ahmed <suraj@geekyants.com>
2021-12-03 11:42:19 +0530

    Modifed rnw form

--- cra-react-native-web/src/App.js
+++ cra-react-native-web/src/App.js
@@ -1,63 +1,56 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { View, Text, TextInput, TouchableOpacity } from "react-native";
-function App() {
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+
+export default function App() {
   return (
-    <View
-      style={{
-        width: "100%",
-        height: "100vh",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View style={styles.container}>
       <View
         style={{
-          backgroundColor: "gray",
-          width: "25%",
-          margin: "auto",
-          padding: "30",
-          borderRadius: "4",
+          flexDirection: "row",
+
+          padding: 20,
         }}
       >
-        <View
-          style={{
-            height: "100%",
-            width: "100%",
-          }}
-        >
-          <View id="email">
-            <Text>Email address</Text>
-            <TextInput type="email" />
-            <Text>We'll never share your email.</Text>
-          </View>
-          <View id="password" pt="8">
-            <Text>Password</Text>
-            <TextInput type="password" />
-          </View>
-          <View
-            style={{
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <TouchableOpacity
-              style={{
-                paddingHorizontal: 10,
-                marginTop: 8,
-                marginHorizontal: "auto",
-                backgroundColor: "blue",
-              }}
-              type="submit"
-              variant="outline"
-            >
-              Submit
-            </TouchableOpacity>
-          </View>
+        <View style={{ backgroundColor: "#f4f4f5", padding: "30px" }}>
+          <Text style={{ color: "black", paddingBottom: "10px" }}>
+            Enter Email
+          </Text>
+          <TextInput style={styles.input} placeholder="Enter Email" />
+          <Text style={{ color: "black", paddingBottom: "10px" }}>
+            Enter Password
+          </Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Enter Password"
+            textContentType="password"
+          />
+          <Button
+            title="Submit"
+            color="black"
+            onPress={() => alert("Submit button pressed")}
+          />
         </View>
       </View>
     </View>
   );
 }
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  input: {
+    height: 40,
+    width: 300,
+    borderColor: "#a1a1aa",
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 4,
+    marginBottom: "20px",
+  },
+});
